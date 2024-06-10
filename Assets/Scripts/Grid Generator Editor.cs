57da9ccf04586e6dd5632a5e1fca3b898da548c6
ciit601pc17 <it@ciit.edu.ph>
2024-06-10 10:41:16 +0800

    v0

---
+++ Assets/Scripts/Grid Generator Editor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using JetBrains.Annotations;
+
+[CustomEditor(typeof(GridGenerator))]
+
+public class GridGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        GridGenerator gridGenerator = (GridGenerator)target;
+        if(GUILayout.Button("Generate Grid"))
+        {
+            gridGenerator.GenerateGrid();
+        }
+        
+        if(GUILayout.Button("Clear Grid"))
+        {
+            gridGenerator.ClearGrid();
+        }
+        
+        if(GUILayout.Button("Assign Material"))
+        {
+            gridGenerator.AssignMaterial();
+        }
+
+    }
+
+    [MenuItem("Tools/Generate Grid")]
+    public static void GenerategridMenu()
+    {
+        GridGenerator gridGenerator = FindObjectOfType<GridGenerator>();
+        if(gridGenerator != null)
+        {
+            gridGenerator.GenerateGrid();
+        }
+
+        else
+        {
+            Debug.LogError("No Grid Generator Found in Scene");
+        }
+    }
+}
